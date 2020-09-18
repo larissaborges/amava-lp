01c24e1f3e5997d3454d204b4e8c52fc8c841057
larissa <larissafurtado12@live.com>
2020-09-18 10:40:23 -0300

    t writer effect

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 import 'bootstrap';
 import './scss/app.scss';
+const Typewriter = require('t-writer.js');
+
+
+const target = document.getElementById('text-animate')
+const writer = new Typewriter(target, {
+    loop: true,
+    typeSpeed: 80,
+    deleteSpeed: 80,
+    typeColor: 'white',
+    cursorColor: 'white'
+  })
+  
+  writer
+    .type('Marketing Digital?')
+    .rest(500)
+    .changeOps({ deleteSpeed: 80 })
+    .remove(18)
+    .type('Landing Pages?')
+    .rest(500)
+    .remove(14)
+    .type('Anúncios?')
+    .rest(500)
+    .changeOps({ deleteSpeed: 80 })
+    .remove(9)
+    .clear()
+    .start()
+        
